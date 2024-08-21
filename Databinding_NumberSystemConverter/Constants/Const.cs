aa148be65dfc81de55818e5674e3688c576f874e
smaldos64 <gudumholm34@gmail.com>
2024-08-21 17:31:49 +0200

    Add project files.

---
+++ Databinding_NumberSystemConverter/Constants/Const.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+using ToolsLibrary;
+
+namespace Databinding_NumberSystemConverter.Constants
+{
+
+    //public struct ValidKeysForNumberInRadixSystems
+    //{
+    //    public Key FirstValidKey;
+    //    public Key SecondValidKey;
+    //    public bool IsKeyALetter;
+
+    //    public ValidKeysForNumberInRadixSystems(Key FirstValidKey, 
+    //                                            Key SecondValidKey,
+    //                                            bool IsKeyALetter)
+    //    {
+    //        this.FirstValidKey = FirstValidKey;
+    //        this.SecondValidKey = SecondValidKey;
+    //        this.IsKeyALetter = IsKeyALetter;
+    //    }
+    //}
+    public class Const
+    {
+        public static readonly int MaxByteValue = 255;
+        public static readonly int NumberOfBitsInByte = 8;
+
+        public static readonly int MinOctetValue = 0;
+        public static readonly int MaxOctetValue = 255;
+
+        public static readonly string NothingEnteredInTextBox = "0";
+
+        public static Key[] ValidSystemsKeyArray =
+        {
+            Key.Delete,
+            Key.Back
+        };
+
+        public static ValidKeysForNumberInRadixSystems[] RadixNumberSystemsValidKeysArray =
+        {
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.D0,
+                SecondValidKey = Key.NumPad0,
+                IsKeyALetter = false
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.D1,
+                SecondValidKey = Key.NumPad1,
+                IsKeyALetter = false
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.D2,
+                SecondValidKey = Key.NumPad2,
+                IsKeyALetter = false
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.D3,
+                SecondValidKey = Key.NumPad3,
+                IsKeyALetter = false
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.D4,
+                SecondValidKey = Key.NumPad4,
+                IsKeyALetter = false
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.D5,
+                SecondValidKey = Key.NumPad5,
+                IsKeyALetter = false
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.D6,
+                SecondValidKey = Key.NumPad6,
+                IsKeyALetter = false
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.D7,
+                SecondValidKey = Key.NumPad7,
+                IsKeyALetter = false
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.D8,
+                SecondValidKey = Key.NumPad8,
+                IsKeyALetter = false
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.D9,
+                SecondValidKey = Key.NumPad9,
+                IsKeyALetter = false
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.A,
+                SecondValidKey = Key.A,
+                IsKeyALetter = true
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.B,
+                SecondValidKey = Key.B,
+                IsKeyALetter = true
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.C,
+                SecondValidKey = Key.C,
+                IsKeyALetter = true
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.D,
+                SecondValidKey = Key.D,
+                IsKeyALetter = true
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.E,
+                SecondValidKey = Key.E,
+                IsKeyALetter = true
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.F,
+                SecondValidKey = Key.F,
+                IsKeyALetter = true
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.G,
+                SecondValidKey = Key.G,
+                IsKeyALetter = true
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.H,
+                SecondValidKey = Key.H,
+                IsKeyALetter = true
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.I,
+                SecondValidKey = Key.I,
+                IsKeyALetter = true
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.J,
+                SecondValidKey = Key.J,
+                IsKeyALetter = true
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.K,
+                SecondValidKey = Key.K,
+                IsKeyALetter = true
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.L,
+                SecondValidKey = Key.L,
+                IsKeyALetter = true
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.M,
+                SecondValidKey = Key.M,
+                IsKeyALetter = true
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.N,
+                SecondValidKey = Key.N,
+                IsKeyALetter = true
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.O,
+                SecondValidKey = Key.O,
+                IsKeyALetter = true
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.P,
+                SecondValidKey = Key.P,
+                IsKeyALetter = true
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.Q,
+                SecondValidKey = Key.Q,
+                IsKeyALetter = true
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.R,
+                SecondValidKey = Key.R,
+                IsKeyALetter = true
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.S,
+                SecondValidKey = Key.S,
+                IsKeyALetter = true
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.T,
+                SecondValidKey = Key.T,
+                IsKeyALetter = true
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.U,
+                SecondValidKey = Key.U,
+                IsKeyALetter = true
+            },
+            new ValidKeysForNumberInRadixSystems()
+            {
+                FirstValidKey = Key.V,
+                SecondValidKey = Key.V,
+                IsKeyALetter = true
+            }
+        };
+    }
+}
---
+++ Databinding_NumberSystemConverter/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Databinding_NumberSystemConverter.Constants;
+using System.Media;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ToolsLibrary;
+
+namespace Databinding_NumberSystemConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtCheckForValidPositiveNumberPressedForRadixSystem(object sender, KeyEventArgs e)
+        {
+            if (!(KeyHelper.IsKeyPressedValidInSpecifiedKeyArray(e.Key, Const.ValidSystemsKeyArray)))
+            {
+                if (!(KeyHelper.IsKeyPressedValidForCurrentNumberSystem(e.Key,
+                                                                        Const.RadixNumberSystemsValidKeysArray,
+                                                                        int.Parse(((TextBox)sender).Tag.ToString()))))
+                {
+                    SystemSounds.Beep.Play();
+                    e.Handled = true;
+                }
+            }
+        }
+    }
+}
